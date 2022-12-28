c9dffb55c718ffce249d735e0bdd22eca9dacaca
prasannaanangi <anangiprasanna@gmail.com>
2022-12-28 11:00:01 +0530

    i am creating registation form

---
+++ src/app/components/registration/registration.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.scss']
+})
+export class RegistrationComponent implements OnInit {
+   RegForm!:FormGroup;
+  constructor() { }
+   
+  ngOnInit(): void {
+  
+  }
+  save(){
+    
+  }
+
+}
