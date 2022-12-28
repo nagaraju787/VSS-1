7c7c8e2902b892dabbe6e3b157660caa991f1628
nagaraju787 <ghantasalanagaraju787@gmail.com>
2022-12-28 18:01:18 +0530

    dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path:"login",component:LoginComponent},
   {path:"registration",pathMatch:"full",component:RegistrationComponent},
+  {path:"dashboard",pathMatch:"full",component:DashboardComponent},
   {path:"",pathMatch:"full",redirectTo:"login"}
 ];
 
