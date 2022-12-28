572b98863f22575eb3345b489ef0d42c4002a993
nagaraju787 <ghantasalanagaraju787@gmail.com>
2022-12-28 11:48:39 +0530

    Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {path:"login",component:LoginComponent},
-  {path:"app",component:AppComponent},
+  {path:"regitration",component:RegistrationComponent},
   {path:"",pathMatch:"full",redirectTo:"login"}
 ];
 
