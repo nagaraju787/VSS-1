13dd5b78ae959361848d80a891a8adba14060a50
nagaraju787 <ghantasalanagaraju787@gmail.com>
2022-12-31 09:34:49 +0530

    userdetails component

--- src/app/userlist/userlist.component.ts
+++ src/app/userlist/userlist.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VssService } from '../vss.service';
 
 @Component({
   selector: 'app-userlist',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserlistComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private vss:VssService) { }
+  booleanvalue:boolean=true;
   ngOnInit(): void {
+    this.vss.passingback.subscribe((value)=>{this.booleanvalue=value})
   }
-
+  users:any=[
+    {name:"Nagaraju", id:1},
+    {name:"PavanKumar Reddy",id:2},
+    {name:"Madhu", id:3},
+    {name:"Singaraiah", id:4},
+    {name:"Prasanna", id:5},
+    {name:"Sunitha", id:6},
+    {name:"Hymavathi", id:7},
+    {name:"Aswani", id:8},
+    {name:"Srilakshmi", id:9},
+    {name:"Moulika", id:10},
+  ];
+    details(){
+      this.booleanvalue=false;
+    }
 }
+
