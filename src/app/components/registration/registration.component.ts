716e79ff3a6dd50d693cdfd87f144a3b090f7988
prasannaanangi <anangiprasanna@gmail.com>
2023-01-05 12:12:22 +0530

    changes

--- src/app/components/registration/registration.component.ts
+++ src/app/components/registration/registration.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { VssService } from 'src/app/vss.service';
 
 @Component({
   selector: 'app-registration',
@@ -9,19 +11,18 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 export class RegistrationComponent implements OnInit {
    RegForm!:FormGroup;
    naga:Boolean= true;
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb:FormBuilder,private vss:VssService,router:Router) { }
    
   ngOnInit(): void {
     this.RegForm = this.fb.group({
-      fname:['',[Validators.required,Validators.minLength(8)]],
-      lname:['',[Validators.required,Validators.minLength(8)]],
+      fname:['',[Validators.required,Validators.minLength(1)]],
+      lname:['',[Validators.required,Validators.minLength(1)]],
       email:['',[Validators.required,Validators.email]],
-      Pswd: ['',[Validators.required,Validators.minLength(8)]],
-      CPswd: ['',[Validators.required,Validators.minLength(8)]],
       phno:['',[Validators.required,Validators.pattern("^[1-9]{10}$"),Validators.maxLength(10)]],
       address:['',[Validators.required,Validators.minLength(20)]],
-      uname:['',[Validators.required,Validators.minLength(8)]],
-      UPswd:['',[Validators.required,Validators.minLength(8)]]
+      uname:['',[Validators.required,Validators.minLength(1)]],
+      pswd: ['',[Validators.required,Validators.minLength(8)]],
+      cpswd: ['',[Validators.required,Validators.minLength(8)]],
     })
   }
   getRegFormControl(key: string):any{
@@ -30,6 +31,16 @@ export class RegistrationComponent implements OnInit {
    save(){
       console.log(this.RegForm.value);
       this.naga= false;
+      const {cpswd, ...data} = this.RegForm.value;
+       this.vss.saveUser(data).subscribe((res)=>{
+         if(res) {
+           return this.RegForm.reset();
+         }
+         else{
+          return false;
+         }
+
+          })
      }
 
   }     
