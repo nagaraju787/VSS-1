5a17ece03aa626c21cfca10b585d065821987f6e
nagaraju787 <ghantasalanagaraju787@gmail.com>
2023-01-08 09:55:34 +0530

    profile icon

--- src/app/vss.service.ts
+++ src/app/vss.service.ts
@@ -3,51 +3,53 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, map, Observable, Subject } from 'rxjs';
 
 
-import {environment} from '../environments/environment'
+import { environment } from '../environments/environment'
 
 @Injectable({
   providedIn: 'root'
 })
 export class VssService {
-  passingback= new BehaviorSubject<any>(true)
-  constructor(private http:HttpClient) { }
+  passingback = new BehaviorSubject<any>(true)
+  Username = new BehaviorSubject<any>(null)
+  constructor(private http: HttpClient) { }
   //for getting invoices
-  getInvoices():Observable<any>{
+  getInvoices(): Observable<any> {
     // return this.http.get("https://jsonplaceholder.typicode.com/users");
     return this.http.get(environment.apiBaseUrl + "invoices")
   }
   saveUser(user: any) {
     console.log(user)
     return this.http.post(environment.apiBaseUrl + 'users', user);
-    
+
   }
   //for deleting invoices
-  deleteInvoice(id:any){
-    return this.http.delete(environment.apiBaseUrl + "invoices/"+id)
+  deleteInvoice(id: any) {
+    return this.http.delete(environment.apiBaseUrl + "invoices/" + id)
   }
   //for adding invoices
-  addInvoice(invoice:any){
+  addInvoice(invoice: any) {
     return this.http.post(environment.apiBaseUrl + 'invoices', invoice);
   }
-  editInvoice(editedInv:any){
-    return this.http.put(environment.apiBaseUrl + "invoices/"+editedInv.id,editedInv)
+  editInvoice(editedInv: any) {
+    return this.http.put(environment.apiBaseUrl + "invoices/" + editedInv.id, editedInv)
   }
 
   //userDetails
-  loginvalidation():Observable<any>{
-    return  this.http.get(environment.apiBaseUrl + "users")
+  loginvalidation(): Observable<any> {
+    return this.http.get(environment.apiBaseUrl + "users")
   }
-login(deta:any):Observable<any>{
-  return this.loginvalidation().pipe(
-    map((users:any)=>{
-  const user=users.find((user:any)=>deta.userName===user.userName&&deta.password===user.password&&deta.userType===user.userType)
-    if(user){
-      return user
-    }else{
-    return {error:"invalid details"}
-    }
-    }))
-}
+  login(deta: any): Observable<any> {
+    return this.loginvalidation().pipe(
+      map((users: any) => {
+        const user = users.find((user: any) => deta.userName === user.userName && deta.password === user.password && deta.userType === user.userType)
+        if (user) {
+          this.Username.next(deta.userName)
+          return user
+        } else {
+          return { error: "invalid details" }
+        }
+      }))
+  }
 
 
 }
