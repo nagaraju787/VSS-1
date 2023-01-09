00a6c318719361faf1065881daeba198ce184949
nagaraju787 <ghantasalanagaraju787@gmail.com>
2023-01-09 08:26:00 +0530

    setintervel carousel

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,13 +9,6 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private config:NgbCarouselConfig) { 
-      // customize default values of carousels used by this component tree
-      config.interval = 2000;
-      config.keyboard = true;
-      config.pauseOnHover = true;
-  }
-
   ngOnInit(): void {
   }
 
